68495264853c6701c4f83f87cfc01cd693974323
Egor_Leontev <egor.leontev@kronos.com>
2017-10-20 10:45:40 +0300

    #1 implement base structure with routing and links

--- app/router.js
+++ app/router.js
@@ -7,6 +7,17 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('main');
+  this.route('cms', function() {
+    this.route('log');
+    this.route('qa_plan');
+    this.route('do_well');
+    this.route('demiurge');
+    this.route('Judgment_bottom');
+    this.route('improvments_list');
+    this.route('recommended_sites');
+    this.route('activists_journal');
+  });
 });
 
 export default Router;
